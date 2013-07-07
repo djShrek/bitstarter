b8e0e818646b9fe359c745c838f52b130b2ec52a
Robert Ho <boho527@gmail.com>
2013-07-07 22:05:27 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var index = fs.readFileSync(index.html);
+var bufferedIndex = buf.toString(index);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(index.html);
+  response.send(bufferedIndex);
 });
 
 var port = process.env.PORT || 5000;

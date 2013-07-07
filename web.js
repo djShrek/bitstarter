d131f7372584dc382c76affbf90f2656ba56bdd9
Robert Ho <boho527@gmail.com>
2013-07-07 21:57:03 +0000

    test1

--- web.js
+++ web.js
@@ -1,12 +1,9 @@
 var express = require('express');
-var fs = require('fs');
-
-var test = fs.readFileSync(index.html);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(test);
+  response.send(index.html);
 });
 
 var port = process.env.PORT || 5000;
